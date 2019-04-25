23d6df6c48b2cd649ab742796ab6a00468ab9bb5
Imatkulov <rustam180492@gmail.com>
2019-04-25 21:35:16 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import javax.sql.rowset.serial.SQLOutputImpl;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Rustam");
-        );
+        int age = 4;
+        if (age == 1) {
+        } else if (age == 1) {
+            System.out.print("Ясли");
+        } else if (age == 2) {
+            System.out.print("Средняя");
+        } else if (age == 3) {
+            System.out.print("Старшая");
+        } else if (age == 4) {
+            System.out.print("подготовительная");
+        }
     }
 }
